727aa1a0b3de2bb5845ffdfcaa24440acd04e302
s4mma3l <lineartnw17@aol.com>
2023-08-21 17:03:19 -0600

    interacion y listas

---
+++ 98866357-main/hogwarts.py
@@ -0,0 +1,9 @@
+estudiante = ["Hermione", "Harry", "Ron"]
+
+print(estudiante[0])
+print(estudiante[1])
+# dentro del codigo podemos realizar la impresion de una forma rudimentaria como esta o podemos hacer un bucle como el siguiente
+print(estudiante[2])
+
+for alumno in estudiante:
+    print(alumno)
